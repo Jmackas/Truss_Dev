abb844a63be4257bfd417007f5b60132a469dae3
James Mackay <jamesrossmackay@gmail.com>
2019-05-03 13:07:09 +0800

    Detect permissions from array

--- custom_permissions/scripts.js
+++ custom_permissions/scripts.js
@@ -2,16 +2,24 @@ adminInterface();
 readOnly();
 
 function adminInterface() {
-    
+    // Detect the username
+    let userName = document.querySelector(".profile-name").innerText;
+
+    // Arrays with all the course advisors
+    let CourseAdvisor = ["JAMES MACKAY", "LINUS HOGAN"];
+
+    for (let i = 1; i < CourseAdvisor; i++) {
+        if (userName == CourseAdvisor[i]) {
+            readOnly();
+        }
+    }
+
 }
 
 function readOnly() {
     let hello = document.querySelectorAll('[value="submit"]');
 
-    console.log(hello[1]);
-
-    for(i = 0; i < hello.length; i++) {
+    for (i = 0; i < hello.length; i++) {
         hello[i].disabled = true;
     }
 }
-
